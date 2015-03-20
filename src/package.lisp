c913790432d022bce1d54d415bc82682b728f316
Masataro Asai <guicho2.71828@gmail.com>
2015-03-20 22:56:36 +0900

    setter definition comes first

--- src/package.lisp
+++ src/package.lisp
@@ -59,6 +59,8 @@ debugging purpose. I assume there won't be so many additional namespaces.
         (declaim (ftype (function ((,type) symbol) (,type)) (setf ,accessor)))
         (declaim (inline ,accessor))
         (declaim (inline (setf ,accessor)))
+        (defun (setf ,accessor) (new-value symbol)
+          (setf (gethash symbol ,hash) new-value))
         (defun ,accessor (symbol &optional (default nil default-supplied-p))
           "Automatically defined getter. When DEFAULT is supplied, the value is set automatically."
           (multiple-value-bind (value found)
@@ -72,8 +74,6 @@ debugging purpose. I assume there won't be so many additional namespaces.
                         (setf (,accessor symbol) default)))))))
         (defun ,boundp (symbol)
           (nth-value 1 (gethash symbol ,hash)))
-        (defun (setf ,accessor) (new-value symbol)
-          (setf (gethash symbol ,hash) new-value))
         ,(when binding
            `(defmacro ,letname (bindings &body body)
               `(namespace-let
